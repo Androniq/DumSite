73e8fc647023d8c4aa2619d02fac6d5c65dff103
Andrij Ghorbunov <androniq@gmail.com>
2018-09-06 00:37:50 +0300

    Refactor

--- src/components/PriorityHeader/PriorityHeader.js
+++ src/components/PriorityHeader/PriorityHeader.js
@@ -19,9 +19,7 @@ class PriorityHeader extends React.Component {
             <span className={s.priorityTitle}>{this.props.priorityTitle}:</span>
             {this.props.popularOverride ? (
                 <span className={s.priorityPopularOverride}>(популярне голосування)</span>
-            ) : (
-              <></>
-            )}
+            ) : ""}
             <span className={s.priorityVoteFor}>{this.props.voteFor}</span>
             <img src="/images/expandArrow.png" className={classnames(s.priorityCollapseIndicator,
                     this.props.isOpen ? s.priorityCollapseIndicatorOpen : s.priorityCollapseIndicatorClose)} />
