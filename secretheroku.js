69e3f1f2a91983793a1fc250423fc69090a372b4
Andrij Ghorbunov <androniq@gmail.com>
2018-09-09 20:21:26 +0300

    Heroku deployment

---
+++ secretheroku.js
@@ -0,0 +1,7 @@
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+export const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
+
+export const SSL_CSR = process.env.SSL_CSR;
+
+export const SSL_PRIVATE_KEY = process.env.SSL_PRIVATE_KEY;
