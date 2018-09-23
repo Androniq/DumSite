448a16ddf86b393788565525cb6971d6ab69a521
Andrij Ghorbunov <androniq@gmail.com>
2018-09-23 19:26:16 +0300

    As far as I understand, SSR is not enabled at all here

--- src/server.mjs
+++ src/server.mjs
@@ -7,16 +7,4 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
-app.get('*', async (req, res, next) =>
-{/*
-    const data = {};
-    data.children = ReactDOM.renderToString(<App />);
-    const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
-    res.status(200);
-    res.send(`<!doctype html>${html}`);*/
-    res.status(200);
-    res.send(`<!doctype html><html><head><title>Ну прівєт, безграмотщина безреактова</title></head><body><h3>Позорище.</h3><script>console.info("Рендернувся")</script></body></html>`);
-
-});
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
