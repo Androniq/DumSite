7b445ff8050cec6ac3a4e09aea1f0f22c4f35e8a
Andrij Ghorbunov <androniq@gmail.com>
2018-09-23 19:07:28 +0300

    Deploy

--- src/server.mjs
+++ src/server.mjs
@@ -1,11 +1,20 @@
 import express from 'express';
-import path from 'path';
+import Html from './components/Html/Html';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+});
+
+app.get('*', async (req, res, next) =>
+{
+    const data = {};
+    data.children = ReactDOM.renderToString(<App />);
+    const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
+    res.status(200);
+    res.send(`<!doctype html>${html}`);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
