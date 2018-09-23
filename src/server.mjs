10d15175f21515d9ccfccd7ae15b5e4c7f45ce21
Andrij Ghorbunov <androniq@gmail.com>
2018-09-23 18:37:06 +0300

    Deploy

--- src/server.mjs
+++ src/server.mjs
@@ -1,7 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
-const port = process.env.SERVER_PORT || 5000;
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
