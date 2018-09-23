6f63467c03aa5c52fda980723a613b6f0f34e749
Andrij Ghorbunov <androniq@gmail.com>
2018-09-23 20:12:54 +0300

    Importing old server code

--- src/server.mjs
+++ src/server.mjs
@@ -1,7 +1,17 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import passport from 'passport';
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
