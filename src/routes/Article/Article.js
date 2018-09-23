bfcd174e92303b7fb3442484627661849f7f5e18
Andrij Ghorbunov <androniq@gmail.com>
2018-09-23 15:03:32 +0300

    Basic API calls

--- src/routes/Article/Article.js
+++ src/routes/Article/Article.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { asyncComponent } from 'react-async-component';
 
 class Article extends React.Component
 {
+    async componentDidMount()
+    {
+        var f = await fetch('/api/hello', { method: 'GET', headers: new Headers({ 'Content-Type': 'application/json' })});
+        console.info(f);
+        var j = await f.json();
+        console.info(j);
+    }
+
     render()
     {
         return (
@@ -10,4 +19,7 @@ class Article extends React.Component
     }
 }
 
-export default Article;
+export default asyncComponent({resolve: () =>
+    {
+        return Article;
+    }});
