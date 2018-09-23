25efc546054c424a41fba895d6e9996d6380dd1a
Andrij Ghorbunov <androniq@gmail.com>
2018-09-24 01:08:36 +0300

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports =
 {
-    mode: "production", // "production" | "development" | "none"  // Chosen mode tells webpack to use its built-in optimizations accordingly.
     entry: "./app/entry", // string | object | array  // defaults to ./src
     // Here the application starts executing
     // and webpack starts bundling
