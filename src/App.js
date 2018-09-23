789774f35c07d79195ac266a2a8b84b5ad0cbfdf
Andrij Ghorbunov <androniq@gmail.com>
2018-09-24 00:12:24 +0300

    START EVERYTHING FROM THE FUCKING SCRATCH

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Switch, Route } from 'react-router';
+import logo from './logo.svg';
 import './App.css';
-import Home from './routes/Home/Home';
-import Article from './routes/Article/Article';
-import Layout from './components/Layout/Layout';
 
-class App extends Component
-{
-    render()
-    {
-      return (
-        <BrowserRouter>
-          <Layout>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/article" component={Article} />
-            </Switch>
-          </Layout>
-        </BrowserRouter>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
