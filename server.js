a21837c28d0d2d6aa546919f89031b57278ce36f
Andriy Gorbunov <andriy.gorbunov@Blackthorn-vision.com>
2018-09-24 15:41:33 +0300

    Squashed react and express into one folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/api/users', (req, res) =>
 {
@@ -10,12 +10,11 @@ app.get('/api/users', (req, res) =>
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
-const defaultPort = process.env.NODE_ENV === 'production' ? 3000 : 3001;
+const defaultPort = process.env.DEVMODE ? 3001 : 3000;
 // if not in production, we should be able to run server and client on different ports simultaneously (and use proxy on client to redirect API calls).
-// Anyway, if process.env.NODE_ENV === 'production', then we probably already have process.env.PORT assigned, so we won't need the default value.
 
 const port = process.env.PORT || defaultPort;
 
