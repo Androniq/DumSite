3652f06136d99d6d436c9b6bd847a117110a039e
Andriy Gorbunov <andriy.gorbunov@Blackthorn-vision.com>
2018-09-24 19:29:42 +0300

    Added HOC to pass props

--- src/routes/Article/Article.js
+++ src/routes/Article/Article.js
@@ -1,38 +1,14 @@
 import React from 'react';
 import { withEverything } from '../../utilities';
 
-const defaultState = { message: "We didn't receive article text from the server yet." };
-
 class Article extends React.Component
 {
-    state = defaultState;
-
-    componentDidMount()
-    {
-        this.refreshData();
-    }
-
-    async componentWillReceiveProps()
-    {
-        await this.setState(defaultState);
-        this.refreshData();
-    }
-
-    async refreshData()
-    {
-        var req = await fetch('/api/article/' + this.props.match.params.id);
-        var json = await req.json();
-        var message = json.message;
-        this.setState({ message });
-    }
-
     render()
     {
-        console.info(this.props);
         return (
             <div>
                 <h2>Article {this.props.match.params.id}:</h2>
-                <p>{this.state.message}</p>
+                <p>{this.props.data.message}</p>
             </div>
             );
     }
