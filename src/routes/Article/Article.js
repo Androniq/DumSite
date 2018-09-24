4e0fa28ace530cc690f79e7e3634b08204a92290
Andriy Gorbunov <andriy.gorbunov@Blackthorn-vision.com>
2018-09-24 18:27:46 +0300

    And more

--- src/routes/Article/Article.js
+++ src/routes/Article/Article.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
+const defaultState = { message: "We didn't receive article text from the server yet." };
+
 export default class Article extends React.Component
 {
+    state = defaultState;
+
+    componentDidMount()
+    {
+        this.refreshData();
+    }
+
+    async componentWillReceiveProps()
+    {
+        await this.setState(defaultState);
+        this.refreshData();
+    }
+
+    async refreshData()
+    {
+        var req = await fetch('/api/article/' + this.props.match.params.id);
+        var json = await req.json();
+        var message = json.message;
+        this.setState({ message });
+    }
+
     render()
     {
-        console.info(this.props);
         return (
             <div>
                 <h2>Article {this.props.match.params.id}:</h2>
-                <p>Lorem ipsum dolor sit amet...</p>
+                <p>{this.state.message}</p>
             </div>
             );
     }
